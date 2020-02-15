b8ff860f6c220f12f6d282c113caf2239104679a
Ican Bachors <bachor.can@gmail.com>
2020-02-15 20:58:24 +0700

    Update screenshot.js

--- handler/screenshot.js
+++ handler/screenshot.js
@@ -1,55 +1,9 @@
-const { parse } = require('url');
-const { parseTarget, endWithCache, endWithError, getInt } = require('../lib/util')('screenshot');
-const chromium = require('../lib/chromium');
+const browser = await puppeteer.launch();
 
-const getScreenshot = async (targetURL, type, quality, fullPage) => {
-    const { browser, page, err } = await chromium.visit(targetURL)
-    if (err) {
-        return { err }
-    }
+const page = await browser.newPage();
+await page.goto('http://bachors.com');
 
-    if (fullPage === undefined) {
-        fullPage = true
-    }
+console.log(await page.content());
+await page.screenshot({path: 'screenshot.png'});
 
-    const file = await page.screenshot({ type,  quality, fullPage });
-    await browser.close();
-    return { file };
-}
-
-module.exports = async function (req, res) {
-    let { target, puppetQuery = {}, err: targetErr} = parseTarget(req)
-    if (targetErr) {
-        return endWithError(res, targetErr)
-    }
-
-    try {
-        let { type = 'png', quality, fullPage } = puppetQuery;
-
-        quality = getInt(quality)
-        if (quality) {
-            if (quality < 0 || 100 < quality) {
-                return endWithError(res, {
-                    code: 400,
-                    type: '400 bad request',
-                    message: `quality must be between 0 and 100 (inclusive). got ${quality} instead.`,
-                })
-            }
-
-            // image type must be jpeg when quality is specified
-            type = 'jpeg'
-        }
-
-        const { file, err: handlerErr } = await getScreenshot(target, type, quality, fullPage);
-        if (handlerErr) {
-            return endWithError(res, handlerErr)
-        }
-
-        return endWithCache(res, 200, `image/${type}`, file)
-    } catch (e) {
-        return endWithError(res, {
-            message: `screenshot failed for target '${target}'.`,
-            errorObject: e,
-        })
-    }
-};
+await browser.close();
